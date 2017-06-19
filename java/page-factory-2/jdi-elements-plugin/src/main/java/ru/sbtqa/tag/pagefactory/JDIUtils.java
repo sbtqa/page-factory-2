50639a88c75defe41fe12b0e72b805378a2b3f8e
Varivoda Ivan <varivoda_ivan@mail.ru>
2017-06-19 13:15:42 +0300

    Added jdi-elements-plugin maven module, change pom dependency version

---
+++ java/page-factory-2/jdi-elements-plugin/src/main/java/ru/sbtqa/tag/pagefactory/JDIUtils.java
@@ -0,0 +1,30 @@
+package ru.sbtqa.tag.pagefactory;
+
+import com.epam.jdi.uitests.web.selenium.elements.WebCascadeInit;
+import com.epam.jdi.uitests.web.settings.WebSettings;
+import org.openqa.selenium.WebDriver;
+import ru.sbtqa.tag.pagefactory.exceptions.FactoryRuntimeException;
+
+import java.io.IOException;
+import java.util.function.Supplier;
+
+/**
+ * Utils methods for jdi-plugin
+ */
+public class JDIUtils {
+    
+    private static String driverName;
+    
+    public static void setJDIConfig(Supplier<WebDriver> driverSupplier)  {
+        try {
+            WebSettings.initFromProperties();
+        } catch (IOException e) {
+            throw new FactoryRuntimeException("Exception occurred during setting jdi", e);
+        }
+        driverName = WebSettings.useDriver(driverSupplier);
+    }
+    
+    public static void initElementsOnPage(Object page) {
+        new WebCascadeInit().initElements(page, driverName);
+    }
+}
