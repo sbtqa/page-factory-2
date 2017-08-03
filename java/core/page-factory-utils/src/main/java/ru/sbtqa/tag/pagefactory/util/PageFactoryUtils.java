0c7d56755483993121217855abd60fbea5423cf1
Surin Anatoliy <savkk@bk.ru>
2017-08-03 11:43:16 +0300

    revert findRedirect

--- java/core/page-factory-utils/src/main/java/ru/sbtqa/tag/pagefactory/util/PageFactoryUtils.java
+++ java/core/page-factory-utils/src/main/java/ru/sbtqa/tag/pagefactory/util/PageFactoryUtils.java
@@ -252,7 +252,7 @@ public class PageFactoryUtils {
                     field.setAccessible(true);
                     Object targetField = field.get(parent);
                     if (targetField != null) {
-                        if (targetField == (element)) {
+                        if (targetField == element) {
                             return redirect.page();
                         }
                     }
