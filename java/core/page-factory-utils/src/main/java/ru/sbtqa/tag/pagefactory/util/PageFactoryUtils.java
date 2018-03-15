2d13bcafb98c61015eedc76b6c34c415da1317bb
Konstantin Maltsev <mkypers@gmail.com>
2018-03-15 11:20:42 +0700

    order pfutils

--- java/core/page-factory-utils/src/main/java/ru/sbtqa/tag/pagefactory/util/PageFactoryUtils.java
+++ java/core/page-factory-utils/src/main/java/ru/sbtqa/tag/pagefactory/util/PageFactoryUtils.java
@@ -7,17 +7,16 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.NoSuchElementException;
 import org.apache.commons.lang3.reflect.MethodUtils;
-import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.sbtqa.tag.allurehelper.ParamsHelper;
 import ru.sbtqa.tag.cucumber.TagCucumber;
 import ru.sbtqa.tag.pagefactory.Page;
-import ru.sbtqa.tag.pagefactory.PageContext;
-import ru.sbtqa.tag.pagefactory.annotations.*;
+import ru.sbtqa.tag.pagefactory.annotations.ActionTitle;
+import ru.sbtqa.tag.pagefactory.annotations.ActionTitles;
+import ru.sbtqa.tag.pagefactory.annotations.ElementTitle;
+import ru.sbtqa.tag.pagefactory.annotations.ValidationRule;
 import ru.sbtqa.tag.pagefactory.exceptions.ElementDescriptionException;
 import ru.sbtqa.tag.pagefactory.exceptions.ElementNotFoundException;
 import ru.sbtqa.tag.pagefactory.exceptions.FactoryRuntimeException;
@@ -29,6 +28,54 @@ import ru.sbtqa.tag.qautils.reflect.FieldUtilsExt;
 public class PageFactoryUtils {
     
     public static final Logger LOG = LoggerFactory.getLogger(PageFactoryUtils.class);
+    
+    /**
+     * Find method with corresponding title on current page, and execute it
+     *
+     * @param page the page on which the method is executing
+     * @param title title of the method to call
+     * @param param parameters that will be passed to method
+     * @throws java.lang.NoSuchMethodException if required method couldn't be
+     * found
+     */
+    public static void executeMethodByTitle(Page page, String title, Object... param) throws NoSuchMethodException {
+        List<Method> methods = getDeclaredMethods(page.getClass());
+        for (Method method : methods) {
+            if (isRequiredAction(method, title)) {
+                try {
+                    method.setAccessible(true);
+                    MethodUtils.invokeMethod(page, method.getName(), param);
+                    return;
+                } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+                    throw new FactoryRuntimeException("Failed to invoke method", e);
+                }
+            }
+        }
+        
+        throw new NoSuchMethodException("There is no '" + title + "' method on '" + page.getTitle() + "' page object");
+    }
+    
+    /**
+     * Return a list of methods declared tin the given class and its super
+     * classes
+     *
+     * @param clazz class to check
+     * @return list of methods. could be empty list
+     */
+    public static List<Method> getDeclaredMethods(Class clazz) {
+        List<Method> methods = new ArrayList<>();
+        
+        methods.addAll(Arrays.asList(clazz.getDeclaredMethods()));
+        
+        Class supp = clazz.getSuperclass();
+        
+        while (supp != java.lang.Object.class) {
+            methods.addAll(Arrays.asList(supp.getDeclaredMethods()));
+            supp = supp.getSuperclass();
+        }
+        
+        return methods;
+    }
     
     /**
      * Check whether given method has {@link ActionTitle} or
@@ -53,7 +100,7 @@ public class PageFactoryUtils {
         for (ActionTitle action : actionList) {
             String actionValue = action.value();
             try {
-                I18N i18n = I18N.getI18n(method.getDeclaringClass(), TagCucumber.getFeature().getI18n().getLocale(), I18N.DEFAULT_BUNDLE_PATH);
+                I18N i18n = I18N.getI18n(method.getDeclaringClass(), TagCucumber.getFeature().getI18n().getLocale());
                 actionValue = i18n.get(action.value());
             } catch (I18NRuntimeException e) {
                 LOG.debug("There is no bundle for translation class. Leave it as is", e);
@@ -67,31 +114,26 @@ public class PageFactoryUtils {
         return false;
     }
     
-    
     /**
-     * Return a list of methods declared tin the given class and its super
-     * classes
+     * Find specified WebElement by title annotation among current page fields
      *
-     * @param clazz class to check
-     * @return list of methods. could be empty list
+     * @param page the page on which the method is executing
+     * @param title title of the element to search
+     * @return WebElement found by corresponding title
+     * @throws ru.sbtqa.tag.pagefactory.exceptions.PageException if failed to
+     * find corresponding element or element type is set incorrectly
      */
-    public static List<Method> getDeclaredMethods(Class clazz) {
-        List<Method> methods = new ArrayList<>();
-        
-        methods.addAll(Arrays.asList(clazz.getDeclaredMethods()));
-        
-        Class supp = clazz.getSuperclass();
-        
-        while (supp != java.lang.Object.class) {
-            methods.addAll(Arrays.asList(supp.getDeclaredMethods()));
-            supp = supp.getSuperclass();
+    public static WebElement getElementByTitle(Page page, String title) throws PageException {
+        for (Field field : FieldUtilsExt.getDeclaredFieldsWithInheritance(page.getClass())) {
+            if (isRequiredElement(field, title)) {
+                return getElementByField(page, field);
+            }
         }
         
-        return methods;
+        throw new ElementNotFoundException(String.format("Element '%s' is not present on current page '%s''", title, page.getTitle()));
     }
     
-    
-    
+        
     /**
      * Check whether {@link ElementTitle} annotation of the field has a
      * required value
@@ -120,8 +162,6 @@ public class PageFactoryUtils {
         return "";
     }
     
-    
-    
     /**
      * Get object from a field of specified parent
      *
@@ -148,66 +188,12 @@ public class PageFactoryUtils {
                     + "' of '" + parentObject.getClass().getCanonicalName() + "'", cce);
         }
     }
-    
-    /**
-     * Find method with corresponding title on current page, and execute it
-     *
-     * @param title title of the method to call
-     * @param param parameters that will be passed to method
-     * @throws java.lang.NoSuchMethodException if required method couldn't be
-     * found
-     */
-    public static void executeMethodByTitle(Page page, String title, Object... param) throws NoSuchMethodException {
-        List<Method> methods = getDeclaredMethods(page.getClass());
-        for (Method method : methods) {
-            if (isRequiredAction(method, title)) {
-                try {
-                    method.setAccessible(true);
-                    MethodUtils.invokeMethod(page, method.getName(), param);
-                    return;
-                } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-                    throw new FactoryRuntimeException("Failed to invoke method", e);
-                }
-            }
-        }
-        
-        throw new NoSuchMethodException("There is no '" + title + "' method on '" + page.getTitle() + "' page object");
-    }
-    
-    /**
-     * Add parameter to allure report.
-     *
-     * @param paramName parameter name
-     * @param paramValue parameter value to set
-     */
-    public static void addToReport(String paramName, String paramValue) {
-        ParamsHelper.addParam(paramName, paramValue);
-        LOG.debug("Add '" + paramName + "->" + paramValue + "' to report for page '"
-                + PageContext.getCurrentPageTitle() + "'");
-    }
-    
-    /**
-     * Find specified WebElement by title annotation among current page fields
-     *
-     * @param title title of the element to search
-     * @return WebElement found by corresponding title
-     * @throws ru.sbtqa.tag.pagefactory.exceptions.PageException if failed to
-     * find corresponding element or element type is set incorrectly
-     */
-    public static WebElement getElementByTitle(Page page, String title) throws PageException {
-        for (Field field : FieldUtilsExt.getDeclaredFieldsWithInheritance(page.getClass())) {
-            if (isRequiredElement(field, title)) {
-                return getElementByField(page, field);
-            }
-        }
-        
-        throw new ElementNotFoundException(String.format("Element '%s' is not present on current page '%s''", title, page.getTitle()));
-    }
-    
+
     /**
      * Find a method with {@link ValidationRule} annotation on current page, and
      * call it
      *
+     * @param page the page on which the method is executing
      * @param title title of the validation rule
      * @param params parameters passed to called method
      * @throws ru.sbtqa.tag.pagefactory.exceptions.PageException if couldn't
@@ -228,38 +214,5 @@ public class PageFactoryUtils {
             }
         }
         throw new PageException("There is no '" + title + "' validation rule in '" + page.getTitle() + "' page.");
-    }
-
-    /**
-     * Search for the given element among the parent object fields,
-     * check whether it has a {@link
-     * RedirectsTo} annotation, and return a redirection page class, if so.
-     * Search goes in recursion if it meets HtmlElement field, as given
-     * element could be inside of the block
-     *
-     * @param element element that is being checked for redirection
-     * @param parent parent object
-     * @return class of the page, this element redirects to
-     */
-    public static Class<? extends Page> findRedirect(Object parent, Object element) {
-        List<Field> fields = FieldUtilsExt.getDeclaredFieldsWithInheritance(parent.getClass());
-
-        for (Field field : fields) {
-            RedirectsTo redirect = field.getAnnotation(RedirectsTo.class);
-            if (redirect != null) {
-                try {
-                    field.setAccessible(true);
-                    Object targetField = field.get(parent);
-                    if (targetField != null) {
-                        if (targetField == element) {
-                            return redirect.page();
-                        }
-                    }
-                } catch (NoSuchElementException | StaleElementReferenceException | SecurityException | IllegalArgumentException | IllegalAccessException ex) {
-                    LOG.debug("Failed to get page destination to redirect for element", ex);
-                }
-            }
-        }
-        return null;
     }
 }
