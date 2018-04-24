b68de24ccb5c4026724bbe393888040b43f78ed3
sbt-markelov-ds <markeloffds@gmail.com>
2018-04-24 08:46:28 +0600

    has been deleted blind

--- core/page-factory-core/src/main/java/ru/sbtqa/tag/pagefactory/support/capabilities/SelenoidCapabilitiesParser.java
+++ core/page-factory-core/src/main/java/ru/sbtqa/tag/pagefactory/support/capabilities/SelenoidCapabilitiesParser.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
-
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
