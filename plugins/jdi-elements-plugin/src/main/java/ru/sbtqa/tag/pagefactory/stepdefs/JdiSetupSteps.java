d35314d8c79954adec60585aea42ee4d4dd30ee5
Сидоченко Виктор Викторович <VVSidochenko.SBT@sberbank.ru>
2018-05-22 13:42:54 +0700

    JDI and mobile plugins has i18n

---
+++ plugins/jdi-elements-plugin/src/main/java/ru/sbtqa/tag/pagefactory/stepdefs/JdiSetupSteps.java
@@ -0,0 +1,20 @@
+package ru.sbtqa.tag.pagefactory.stepdefs;
+
+import ru.sbtqa.tag.pagefactory.environment.Environment;
+import ru.sbtqa.tag.pagefactory.jdi.actions.JdiPageActions;
+import ru.sbtqa.tag.pagefactory.jdi.utils.JDIUtils;
+
+public class JdiSetupSteps {
+
+    private static final ThreadLocal<Boolean> isJdiInited = ThreadLocal.withInitial(() -> false);
+
+    public synchronized void initJDI() {
+        if (isJdiInited.get()) {
+            return;
+        } else {
+            isJdiInited.set(true);
+        }
+        Environment.setPageActions(new JdiPageActions());
+        JDIUtils.setJDIConfig(() -> Environment.getDriverService().getDriver());
+    }
+}
--- plugins/mobile-plugin/src/main/java/ru/sbtqa/tag/pagefactory/stepdefs/MobileSetupSteps.java
+++ plugins/mobile-plugin/src/main/java/ru/sbtqa/tag/pagefactory/stepdefs/MobileSetupSteps.java
@@ -1,15 +1,21 @@
 package ru.sbtqa.tag.pagefactory.stepdefs;
 
-import cucumber.api.java.Before;
-import ru.sbtqa.tag.pagefactory.mobile.drivers.MobileDriverService;
 import ru.sbtqa.tag.pagefactory.environment.Environment;
 import ru.sbtqa.tag.pagefactory.mobile.actions.MobilePageActions;
 import ru.sbtqa.tag.pagefactory.mobile.checks.MobilePageChecks;
+import ru.sbtqa.tag.pagefactory.mobile.drivers.MobileDriverService;
 
-public class MobileSetupStepDefs {
+public class MobileSetupSteps {
 
-    @Before(order = 1)
-    public void setUp() {
+
+    private static final ThreadLocal<Boolean> isMobileInited = ThreadLocal.withInitial(() -> false);
+
+    public synchronized void initMobile() {
+        if (isMobileInited.get()) {
+            return;
+        } else {
+            isMobileInited.set(true);
+        }
         Environment.setDriverService(new MobileDriverService());
         Environment.setPageActions(new MobilePageActions());
         Environment.setPageChecks(new MobilePageChecks());
