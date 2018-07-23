55df65f765de9adf5233b1d03647a755248a83e8
Konstantin Maltsev <mkypers@gmail.com>
2018-07-23 15:38:39 +0700

    search fragment by scenario name

---
+++ page-factory-core/src/main/java/ru/sbtqa/tag/pagefactory/fragments/FragmentCacheUtils.java
@@ -0,0 +1,42 @@
+package ru.sbtqa.tag.pagefactory.fragments;
+
+import cucumber.runtime.model.CucumberFeature;
+import gherkin.ast.Feature;
+import gherkin.ast.GherkinDocument;
+import gherkin.ast.ScenarioDefinition;
+import gherkin.ast.Step;
+import gherkin.ast.Tag;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import ru.sbtqa.tag.pagefactory.utils.ReflectionUtils;
+
+public class FragmentCacheUtils {
+
+    private static final String FRAGMENT_TAG = "@fragment";
+
+    private FragmentCacheUtils() {}
+
+    public static Map<String, List<Step>> cacheFragments(List<CucumberFeature> features) {
+        Map<String, List<Step>> fragments = new HashMap<>();
+
+        for (CucumberFeature cucumberFeature : features) {
+            GherkinDocument gherkinDocument = cucumberFeature.getGherkinFeature();
+            Feature feature = gherkinDocument.getFeature();
+            List<ScenarioDefinition> scenarioDefinitions = feature.getChildren();
+            for (ScenarioDefinition scenarioDefinition : scenarioDefinitions) {
+                List<Tag> tags = ReflectionUtils.getScenarioTags(scenarioDefinition);
+                if (isFragmentTagContains(tags)) {
+                    fragments.put(scenarioDefinition.getName(), scenarioDefinition.getSteps());
+                }
+            }
+        }
+
+        return fragments;
+    }
+
+    private static boolean isFragmentTagContains(List<Tag> tags) {
+        return tags.stream().anyMatch(tag -> tag.getName().equals(FRAGMENT_TAG));
+    }
+
+}
--- plugins/web-plugin/src/test/resources/fragments/actions.feature
+++ plugins/web-plugin/src/test/resources/fragments/actions.feature
@@ -2,12 +2,12 @@
 @fragments
 Feature: actions fragments
 
-  @fragment=click_fragment
+  @fragment
   Scenario: click fragment
     * user (click the button) "send"
     * user (check that error message contains) "Please specify your first name"
 
-  @fragment=fill_fragment
+  @fragment
   Scenario: fill fragment
     * user (fill the field) "first name" "<first name>"
     * user (click the button) "<button name>"
