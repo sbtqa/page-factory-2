01730f7db2cfb5962fec6b9ff6617481a94152f7
Konstantin Maltsev <mkypers@gmail.com>
2018-10-08 15:04:00 +0700

    fix plugin pages

--- plugins/jdi-elements-plugin/src/main/java/ru/sbtqa/tag/pagefactory/JDIPage.java
+++ plugins/jdi-elements-plugin/src/main/java/ru/sbtqa/tag/pagefactory/JDIPage.java
@@ -6,7 +6,7 @@ import ru.sbtqa.tag.pagefactory.jdi.utils.JDIUtils;
  * Contains basic ru.sbtqa.tag.pagefactory.mobile.actions in particular with jdi elements
  * If we want to extend this functional - inherit from this class
  */
-public abstract class JDIPage extends DefaultPage {
+public abstract class JDIPage implements Page {
 
     public JDIPage() {
         JDIUtils.initElementsOnPage(this);
--- plugins/mobile-plugin/src/main/java/ru/sbtqa/tag/pagefactory/mobile/MobilePage.java
+++ plugins/mobile-plugin/src/main/java/ru/sbtqa/tag/pagefactory/mobile/MobilePage.java
@@ -1,8 +1,8 @@
 package ru.sbtqa.tag.pagefactory.mobile;
 
-import ru.sbtqa.tag.pagefactory.DefaultPage;
+import ru.sbtqa.tag.pagefactory.Page;
 
 /**
  * Inherit your mobile page objects from this class
  */
-public abstract class MobilePage extends DefaultPage {}
+public abstract class MobilePage implements Page {}
