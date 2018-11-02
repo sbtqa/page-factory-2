6b330077c7e08698b5ae6bc67c31befcde7eb3f2
Konstantin Maltsev <mkypers@gmail.com>
2018-11-02 13:49:39 +0700

    add private constructor

--- page-factory-core/src/main/java/ru/sbtqa/tag/pagefactory/Tag.java
+++ page-factory-core/src/main/java/ru/sbtqa/tag/pagefactory/Tag.java
@@ -5,6 +5,8 @@ import ru.sbtqa.tag.pagefactory.environment.Environment;
 
 public class Tag {
 
+    private Tag() {}
+
     @AfterClass
     public static void tearDown() {
         if (!Environment.isDriverEmpty()) {
