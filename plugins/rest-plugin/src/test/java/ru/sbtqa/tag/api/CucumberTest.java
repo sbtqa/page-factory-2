4b52b1b85befb166569cb1df47b5c7527d0d17ce
Konstantin Maltsev <mkypers@gmail.com>
2018-11-30 10:54:32 +0700

    test sonar

--- plugins/rest-plugin/src/test/java/ru/sbtqa/tag/api/CucumberTest.java
+++ plugins/rest-plugin/src/test/java/ru/sbtqa/tag/api/CucumberTest.java
@@ -13,6 +13,7 @@ import ru.sbtqa.tag.api.utils.JettyServiceUtils;
         glue = {"ru.sbtqa.tag.stepdefs"},
         features = {"src/test/resources/features"},
         plugin = {"pretty"}
+        ,tags = {"@dаtatable"}
 )
 public class CucumberTest {
 
