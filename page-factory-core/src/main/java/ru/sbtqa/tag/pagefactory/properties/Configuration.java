3f033a216a60c147324413defaaafcf7451a2d67
Сидоченко Виктор Викторович <VVSidochenko.SBT@sberbank.ru>
2019-01-10 19:14:29 +0700

    Fix sonar issues

--- page-factory-core/src/main/java/ru/sbtqa/tag/pagefactory/properties/Configuration.java
+++ page-factory-core/src/main/java/ru/sbtqa/tag/pagefactory/properties/Configuration.java
@@ -2,9 +2,6 @@ package ru.sbtqa.tag.pagefactory.properties;
 
 import org.aeonbits.owner.Config;
 import org.aeonbits.owner.ConfigFactory;
-import org.aeonbits.owner.Factory;
-import org.aeonbits.owner.Config.LoadType;
-import org.aeonbits.owner.Config.Sources;
 
 import ru.sbtqa.tag.qautils.properties.Props;
 public interface Configuration extends Config {
