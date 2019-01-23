06c16ac9bee0cec77f175af8a05280859c24887d
Сидоченко Виктор Викторович <VVSidochenko.SBT@sberbank.ru>
2019-01-23 11:54:24 +0700

    Make screenshot out of after

--- page-factory-core/src/main/java/ru/sbtqa/tag/pagefactory/aspects/report/AttachScreenshot.java
+++ page-factory-core/src/main/java/ru/sbtqa/tag/pagefactory/aspects/report/AttachScreenshot.java
@@ -1,14 +1,13 @@
 package ru.sbtqa.tag.pagefactory.aspects.report;
 
+import cucumber.api.Result;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.sbtqa.tag.allurehelper.ParamsHelper;
 import ru.sbtqa.tag.allurehelper.Type;
-import ru.sbtqa.tag.pagefactory.context.ScenarioContext;
-import ru.sbtqa.tag.pagefactory.environment.Environment;
 import ru.sbtqa.tag.pagefactory.properties.Configuration;
 import ru.sbtqa.tag.pagefactory.utils.ScreenshotUtils;
 
@@ -18,14 +17,6 @@ public class AttachScreenshot {
     private static final Configuration PROPERTIES = Configuration.create();
     private static final Logger LOG = LoggerFactory.getLogger(AttachScreenshot.class);
 
-    @Pointcut("execution(* ru.sbtqa.tag.stepdefs.CoreSetupSteps.tearDown()) && if()")
-    public static boolean attachScreenshotOnTearDown() {
-        return ScenarioContext.getScenario() != null
-                && ScenarioContext.getScenario().isFailed()
-                && !Environment.isDriverEmpty();
-    }
-
-    @Before("attachScreenshotOnTearDown()")
     public void attach() {
         try {
             ScreenshotUtils screenshot = ScreenshotUtils.valueOf(PROPERTIES.getScreenshotStrategy().toUpperCase());
@@ -33,5 +24,14 @@ public class AttachScreenshot {
         } catch (Exception e) {
             LOG.error("Can't attach screenshot to allure reports", e);
         }
+    }
+
+    @Around("call(* cucumber.runtime.junit.JUnitReporter.handleStepResult(..))")
+    public Object errorAspect(ProceedingJoinPoint joinPoint) throws Throwable {
+        final Object arg = (Result) joinPoint.getArgs()[1];
+        if (((Result) arg).getStatus() == Result.Type.FAILED) {
+            attach();
+        }
+        return joinPoint.proceed();
     }
 }
