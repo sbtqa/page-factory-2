9c8791ecf714e8ec55838491f3cb4b833d016c86
Konstantin Maltsev <mkypers@gmail.com>
2019-01-23 14:54:27 +0700

    update doc

--- plugins/html-plugin/src/main/java/ru/sbtqa/tag/pagefactory/HTMLPage.java
+++ plugins/html-plugin/src/main/java/ru/sbtqa/tag/pagefactory/HTMLPage.java
@@ -3,8 +3,10 @@ package ru.sbtqa.tag.pagefactory;
 import org.openqa.selenium.support.pagefactory.FieldDecorator;
 import ru.sbtqa.tag.pagefactory.actions.PageActions;
 import ru.sbtqa.tag.pagefactory.environment.Environment;
+import ru.sbtqa.tag.pagefactory.find.HtmlFindUtils;
 import ru.sbtqa.tag.pagefactory.html.actions.HtmlPageActions;
 import ru.sbtqa.tag.pagefactory.html.loader.decorators.CustomHtmlElementDecorator;
+import ru.sbtqa.tag.pagefactory.reflection.HtmlReflection;
 import ru.yandex.qatools.htmlelements.loader.decorator.HtmlElementLocatorFactory;
 
 /**
@@ -16,7 +18,10 @@ public abstract class HTMLPage extends WebPage {
 
     public HTMLPage() {
         super(new CustomHtmlElementDecorator(new HtmlElementLocatorFactory(Environment.getDriverService().getDriver())));
+
         Environment.setPageActions(pageActions);
+        Environment.setReflection(new HtmlReflection());
+        Environment.setFindUtils(new HtmlFindUtils());
     }
 
     public HTMLPage(FieldDecorator decorator) {
