19134e854673599f6db8e1a399924c35b8998e67
Konstantin Maltsev <mkypers@gmail.com>
2019-02-18 13:53:17 +0700

    test props via travis env

--- plugins/mobile-plugin/src/main/java/ru/sbtqa/tag/pagefactory/mobile/drivers/MobileDriverService.java
+++ plugins/mobile-plugin/src/main/java/ru/sbtqa/tag/pagefactory/mobile/drivers/MobileDriverService.java
@@ -22,7 +22,7 @@ public class MobileDriverService implements DriverService {
 
     @Override
     public void mountDriver() {
-        System.out.println("QQQQQQQ=" + System.getenv("SAUCELABS_TOKEN"));
+        System.out.println("QQQQQQQ=" + System.getenv("appium.url"));
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("app", PROPERTIES.getAppiumApp());
