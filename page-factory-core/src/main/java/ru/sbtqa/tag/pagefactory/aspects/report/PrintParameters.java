a7c98602d7073395f3d1fd02a5124f78f227b371
Unknown <SandraSands38@gmail.com>
2019-02-18 16:49:30 +0700

    added print pickleTable and pickleString

---
+++ page-factory-core/src/main/java/ru/sbtqa/tag/pagefactory/aspects/report/PrintParameters.java
@@ -0,0 +1,15 @@
+package ru.sbtqa.tag.pagefactory.aspects.report;
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>$Title$</title>
+</head>
+<body>
+$END$
+</body>
+</html>
+
+public class PrintParameters {
+}
