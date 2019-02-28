0b6484832bdc9fb85d23fc4ed47ed14628334b5d
Viktor Sidochenko <viktor.sidochenko@gmail.com>
2019-02-28 18:19:45 +0700

    Add snapshots support

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,9 @@
+DOCS_DIR=master
+
 if [ ! -z "$TRAVIS_TAG" ]
 then
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
+    $DOCS_DIR=$TRAVIS_TAG
     mvn --settings $TRAVIS_BUILD_DIR/.travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
 else
     echo "not on a tag -> keep snapshot version in pom.xml"
@@ -8,10 +11,11 @@ fi
 
 mvn clean deploy --settings $TRAVIS_BUILD_DIR/.travis/settings.xml -DskipTests=true -Drelease=true -B -U
 
-git clone https://github.com/sbtqa/page-gactory-2-site.git
-mkdir -p page-gactory-2-site/releases/$TRAVIS_TAG
-cp -r page-factory-doc/target/doc/index.html page-factory-doc/target/doc/images/ page-gactory-2-site/releases/$TRAVIS_TAG/
-cd page-factory-site/
+git clone https://github.com/sbtqa/page-factory-2-site.git
+mkdir -p page-factory-2-site/releases/$DOCS_DIR
+rm -rf page-factory-2-site/releases/$DOCS_DIR/*
+cp -r page-factory-doc/target/doc/index.html page-factory-doc/target/doc/images/ page-factory-2-site/releases/$DOCS_DIR/
+cd page-factory2-site/
 git add -A
-git ci -m "Add doc for ${TRAVIS_TAG} release"
+git ci -m "Add docs for ${DOCS_DIR} release"
 git push
