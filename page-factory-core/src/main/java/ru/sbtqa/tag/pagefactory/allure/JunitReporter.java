41b674203692af051898982176862bd3ca589fa9
Konstantin Maltsev <mkypers@gmail.com>
2019-03-15 09:28:29 +0700

    add some refact

--- page-factory-core/src/main/java/ru/sbtqa/tag/pagefactory/allure/JunitReporter.java
+++ page-factory-core/src/main/java/ru/sbtqa/tag/pagefactory/allure/JunitReporter.java
@@ -20,17 +20,21 @@ public class JunitReporter {
         boolean isFromCucumber = Arrays.stream(
                 Thread.currentThread().getStackTrace()
         ).anyMatch(stackTraceElement -> stackTraceElement.getClassName().matches("ru\\.sbtqa\\.tag\\.stepdefs\\.[en.|ru.].*"));
+
         if (isFromCucumber) {
             return joinPoint.proceed();
         } else {
             Object[] args = normalizeArgs(joinPoint.getArgs());
-            String stepUid = joinPoint.getSignature().toLongString() + Arrays.toString(args) + System.currentTimeMillis();
             String methodName = joinPoint.getSignature().getName();
+            // I18n contains template for steps as <methodName><dot><argsCount>. For example: fill.2
             String methodNameWithArgsCount = methodName + "." + args.length;
-            String stepNameI18n = I18N.getI18n(joinPoint.getSignature().getDeclaringType(), Locale.forLanguageTag(PROPERTIES.getJunitLang())).get(methodNameWithArgsCount);
-            String stepName = String.format((stepNameI18n.equals(methodNameWithArgsCount) ? methodName : stepNameI18n), args);
 
+            String stepUid = createUid(joinPoint);
+            String stepNameI18n = getStepNameI18n(joinPoint, methodNameWithArgsCount);
+            // if step has i18n template - substitute args to it
+            String stepName = String.format((stepNameI18n.equals(methodNameWithArgsCount) ? methodName : stepNameI18n), args);
             Allure.getLifecycle().startStep(stepUid, new StepResult().setName(stepName));
+
             try {
                 Object r = joinPoint.proceed();
                 Allure.getLifecycle().updateStep(stepUid, stepResult -> stepResult.setStatus(Status.PASSED));
@@ -40,17 +44,36 @@ public class JunitReporter {
                         stepResult.setStatus(Status.FAILED).setStatusDetails(new StatusDetails().setTrace(ExceptionUtils.getStackTrace(t)).setMessage(t.getMessage())));
                 throw t;
             } finally {
-                attachParameters(methodName, args, joinPoint.getSignature().toLongString(), stepName);
+                attachParameters(methodName, args, createUid(joinPoint), stepName);
                 Allure.getLifecycle().stopStep();
             }
         }
     }
 
+    private static String getStepNameI18n(ProceedingJoinPoint joinPoint, String method) {
+        Locale locale = Locale.forLanguageTag(PROPERTIES.getJunitLang());
+        Class clazz = joinPoint.getSignature().getDeclaringType();
+
+        I18N i18n = I18N.getI18n(clazz, locale);
+
+        return i18n.get(method);
+    }
+
+    private static String createUid(ProceedingJoinPoint joinPoint) {
+        return joinPoint.getSignature().toLongString()
+                + Arrays.toString(normalizeArgs(joinPoint.getArgs()))
+                + System.currentTimeMillis();
+    }
+
     private static Object[] normalizeArgs(Object[] args) {
         return Arrays.stream(args).map(arg -> {
+
+            // In case of (action) we have args as Object... and it is array. We need it as String
             if (arg instanceof Object[]) {
                 arg = Arrays.toString((Object[]) arg);
             }
+
+            // In case of CONDITION.POSITIVE arg is null and we need at as empty String
             return arg.toString() == null ? "" : arg;
         }).toArray();
     }
