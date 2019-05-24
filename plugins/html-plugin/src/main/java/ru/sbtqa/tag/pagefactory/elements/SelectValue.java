91b5164c199011284ce3c77c47f6e8721b2514fc
Unknown <SandraSands38@gmail.com>
2019-05-24 14:20:01 +0700

    added component: control

---
+++ plugins/html-plugin/src/main/java/ru/sbtqa/tag/pagefactory/elements/SelectValue.java
@@ -0,0 +1,17 @@
+package ru.sbtqa.tag.pagefactory.elements;
+
+import org.openqa.selenium.WebElement;
+
+/**
+ * Interface for elements with a choice of values
+ */
+public interface SelectValue extends WebElement {
+
+    /**
+     * Searches for a radio button for a given text and selects it.
+     * If the button is locked, an error will be thrown
+     *
+     * @param value text of the button to be selected
+     */
+    void selectByValue(String value);
+}
