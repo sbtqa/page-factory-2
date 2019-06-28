d9624205c3f5af19c9d1e0729b65155818b82cf1
Viktor Sidochenko <viktor.sidochenko@gmail.com>
2019-06-28 15:36:47 +0700

    Fix pr issues

--- plugins/rest-plugin/src/test/java/ru/sbtqa/tag/api/steps/MyStepdefs.java
+++ plugins/rest-plugin/src/test/java/ru/sbtqa/tag/api/steps/MyStepdefs.java
@@ -2,20 +2,15 @@ package ru.sbtqa.tag.api.steps;
 
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
+
+import static java.lang.String.format;
 
 public class MyStepdefs {
-    public MyStepdefs() {
-    }
-
-    @When("^print text \"([^\"]*)\"$")
-    public void println(String arg0) {
-        System.out.println(arg0);
-    }
 
     @And("^print text \"([^\"]*)\" , \"([^\"]*)\" , \"([^\"]*)\"$")
-    public void printText(String arg0, String arg1, String arg2) {
-        System.out.println(String.format("1=%s, \n 2=%s, \n 3=%s", arg0, arg1, arg2));
-
+    public void checkData(String brand, String year, String model) {
+        Assert.assertEquals("BMW 3er 2014", format("%s %s %s", brand, model, year));
     }
 }
 
