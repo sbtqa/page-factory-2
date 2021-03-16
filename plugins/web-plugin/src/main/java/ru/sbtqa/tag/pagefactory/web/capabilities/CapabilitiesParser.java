b57be23c73333a731652ca12e6fee6e97c48ca3f
Konstantin Maltsev <maltsevky@gmail.com>
2021-03-16 14:21:44 +0700

    remove cap parser interface from web-plugin

--- plugins/web-plugin/src/main/java/ru/sbtqa/tag/pagefactory/web/capabilities/CapabilitiesParser.java
+++
@@ -1,8 +0,0 @@
-package ru.sbtqa.tag.pagefactory.web.capabilities;
-
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-public interface CapabilitiesParser {
-    
-    DesiredCapabilities parse();
-}
