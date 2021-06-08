bc505d537990c0078dfc9aba51359e3013c0b1b3
Viktor Sidochenko <viktor.sidochenko@gmail.com>
2021-06-08 19:07:31 +0700

    Fix versions sort

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -20,7 +20,7 @@ mkdir -p $DOCS_RELEASES_DIR
 rm -rf $DOCS_RELEASES_DIR/*
 cp -r page-factory-doc/target/doc/index.html page-factory-doc/target/doc/images/ $DOCS_RELEASES_DIR/
 cd $DOCS_RELEASES_DIR
-ls -tp|tail -n +8| xargs -I {} rm -rf -- {}
+ls -1p |sort -V|tail -n +8| xargs -I {} rm -rf -- {}
 cd ../../
 curl https://${GITHUB_AUTH_TOKEN}@api.github.com/repositories/172893709/contents/releases -o releases.json
 pwd
