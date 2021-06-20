805da09069d793a03b7f1e928b712ae3e7b17fce
Viktor Sidochenko <viktor.sidochenko@gmail.com>
2021-06-20 14:41:16 +0700

    Add test to show how to fill an array with separate custom step definition

--- plugins/rest-plugin/src/test/java/ru/sbtqa/tag/api/steps/MyStepdefs.java
+++ plugins/rest-plugin/src/test/java/ru/sbtqa/tag/api/steps/MyStepdefs.java
@@ -1,8 +1,9 @@
 package ru.sbtqa.tag.api.steps;
 
 import cucumber.api.java.en.And;
-import cucumber.api.java.en.When;
+import io.cucumber.datatable.DataTable;
 import org.junit.Assert;
+import ru.sbtqa.tag.datajack.Stash;
 
 import static java.lang.String.format;
 
@@ -11,6 +12,12 @@ public class MyStepdefs {
     @And("^print text \"([^\"]*)\" , \"([^\"]*)\" , \"([^\"]*)\"$")
     public void checkData(String brand, String year, String model) {
         Assert.assertEquals("BMW 3er 2014", format("%s %s %s", brand, model, year));
+    }
+
+    @And("^user prepares a data array to stash value \"([^\"]*)\":$")
+    public void prepareArray(String stashValueName, DataTable data) {
+        String array = String.join(",", data.asList());
+        Stash.put(stashValueName, array);
     }
 }
 
