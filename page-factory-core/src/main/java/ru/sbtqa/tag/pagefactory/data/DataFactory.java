b33bea322e121a4e9f4f8c7d21fcec3365f45ebc
Viktor Sidochenko <viktor.sidochenko@gmail.com>
2021-11-10 10:52:49 +0700

    Add ability to set custom test data provider
    via #356

--- page-factory-core/src/main/java/ru/sbtqa/tag/pagefactory/data/DataFactory.java
+++ page-factory-core/src/main/java/ru/sbtqa/tag/pagefactory/data/DataFactory.java
@@ -1,11 +1,7 @@
 package ru.sbtqa.tag.pagefactory.data;
 
-import com.mongodb.DB;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
-
-import java.lang.reflect.InvocationTargetException;
-
 import com.mongodb.client.MongoDatabase;
 import org.apache.commons.lang3.reflect.ConstructorUtils;
 import org.slf4j.Logger;
@@ -15,6 +11,8 @@ import ru.sbtqa.tag.datajack.callback.GeneratorCallback;
 import ru.sbtqa.tag.datajack.exceptions.DataException;
 import ru.sbtqa.tag.pagefactory.properties.Configuration;
 
+import java.lang.reflect.InvocationTargetException;
+
 import static java.lang.String.format;
 
 public class DataFactory {
@@ -22,7 +20,7 @@ public class DataFactory {
     private static final Logger LOG = LoggerFactory.getLogger(DataFactory.class);
     private static final Configuration PROPERTIES = Configuration.create();
     private static TestDataProvider testDataProvider;
-    private static final String BASE_FQDN = "ru.sbtqa.tag.datajack.providers.";
+    private static final String BASE_FQDN = PROPERTIES.getDataProviderFqdn() + ".";
 
     private enum PROVIDERS {
         JSON_DATA_PROVIDER("json.JsonDataProvider"),
@@ -85,7 +83,15 @@ public class DataFactory {
                     LOG.debug("Data provider isn't set. Leaving all placeholders as is.");
                     break;
                 default:
-                    throw new DataException(format("Data provider %s isn't supported", dataType));
+                    LOG.debug(format("Using custom data provider %s", dataType));
+                    if (!dataExtension.isEmpty() && !PROPERTIES.getDataDb().isEmpty()) {
+                        throw new DataException("data.extension and data.db could not be set both");
+                    }
+                    testDataProvider = initProvider(dataType,
+                            dataFolder,
+                            initialCollection,
+                            dataExtension+PROPERTIES.getDataDb()
+                    );
             }
         }
         if (!PROPERTIES.getGeneratorsClass().isEmpty()) {
@@ -106,12 +112,16 @@ public class DataFactory {
     }
 
     private static TestDataProvider initProvider(PROVIDERS provider, Object... args) throws DataException {
+        return initProvider(provider.value, args);
+    }
+
+    private static TestDataProvider initProvider(String provider, Object... args) throws DataException {
         try {
-            Class<?> providerClass = DataFactory.class.getClassLoader().loadClass(BASE_FQDN + provider.value);
+            Class<?> providerClass = DataFactory.class.getClassLoader().loadClass(BASE_FQDN + provider);
             return (TestDataProvider) ConstructorUtils.invokeConstructor(providerClass, args);
         } catch (ClassNotFoundException e) {
             throw new DataException(format("Could not find data provider %s in classpath. " +
-                    "Make sure you're added required maven dependency", provider.getName()));
+                    "Make sure you're added required maven dependency", provider));
         } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException ex) {
             throw new DataException(format("Could not initialize data provider %s", provider), ex);
         }
