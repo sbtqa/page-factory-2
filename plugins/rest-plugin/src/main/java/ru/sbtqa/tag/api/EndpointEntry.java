b653a3856305a79042c086b21954c4ac09c66e10
Konstantin Maltsev <maltsevky@gmail.com>
2021-11-19 08:24:43 +0700

    add path getter and setter

--- plugins/rest-plugin/src/main/java/ru/sbtqa/tag/api/EndpointEntry.java
+++ plugins/rest-plugin/src/main/java/ru/sbtqa/tag/api/EndpointEntry.java
@@ -44,7 +44,6 @@ public class EndpointEntry implements ApiEndpoint {
     public EndpointEntry() {
         Endpoint endpoint = this.getClass().getAnnotation(Endpoint.class);
         method = endpoint.method();
-        host = endpoint.host();
         path = endpoint.path();
         template = endpoint.template();
         title = endpoint.title();
@@ -72,7 +71,7 @@ public class EndpointEntry implements ApiEndpoint {
         reflection.applyAnnotations(Query.class);
         reflection.applyAnnotations(Stashed.class);
         reflection.applyAnnotations(Mutator.class);
-        String url = PathUtils.unite(host.isEmpty() ? PROPERTIES.getBaseURI() : host, path);
+        String url = PathUtils.unite(host == null || host.isEmpty() ? PROPERTIES.getBaseURI() : host, path);
 
         RequestSpecification request = buildRequest();
         Response response;
@@ -192,6 +191,14 @@ public class EndpointEntry implements ApiEndpoint {
 
     public void setPath(String path) {
         this.path = path;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
     }
 
     @Override
