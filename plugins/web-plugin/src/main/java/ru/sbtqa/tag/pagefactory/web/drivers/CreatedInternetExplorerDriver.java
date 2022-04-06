94756153838d6bef5a6d170043d40d6283d4730d
hooligan4ik95 <artem-lstik@mail.ru>
2022-04-06 14:09:33 +0300

    Add options to IE

--- plugins/web-plugin/src/main/java/ru/sbtqa/tag/pagefactory/web/drivers/CreatedInternetExplorerDriver.java
+++ plugins/web-plugin/src/main/java/ru/sbtqa/tag/pagefactory/web/drivers/CreatedInternetExplorerDriver.java
@@ -3,6 +3,7 @@ package ru.sbtqa.tag.pagefactory.web.drivers;
 import io.github.bonigarcia.wdm.InternetExplorerDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import ru.sbtqa.tag.pagefactory.web.configure.WebDriverManagerConfigurator;
 import ru.sbtqa.tag.pagefactory.web.support.BrowserName;
@@ -11,15 +12,16 @@ import java.util.function.Supplier;
 
 public class CreatedInternetExplorerDriver implements Supplier<WebDriver> {
 
-    private final DesiredCapabilities capabilities;
+    private final InternetExplorerOptions options;
 
     public CreatedInternetExplorerDriver(final DesiredCapabilities capabilities) {
-        this.capabilities = capabilities;
+        options = new InternetExplorerOptions();
+        options.merge(capabilities);
     }
 
     @Override
     public WebDriver get() {
         WebDriverManagerConfigurator.configureDriver(InternetExplorerDriverManager.getInstance(), BrowserName.IE.getName());
-        return new InternetExplorerDriver(capabilities);
+        return new InternetExplorerDriver(options);
     }
 }
