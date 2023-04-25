7061d00dc5fc17ab381871647d7e53b7bdfa78f5
Konstantin Maltsev <maltsevky@gmail.com>
2023-04-25 14:08:24 +0700

    try gihub-token

--- .travis/pushback.sh
+++ .travis/pushback.sh
@@ -3,10 +3,10 @@ set -e
 
 if [ ! -z "$TRAVIS_TAG" ]
 then
-  git clone https://$GITHUB_AUTH_TOKEN@github.com/sbtqa/page-factory-2-example.git
+  git clone https://$GITHUB_TOKEN@github.com/sbtqa/page-factory-2-example.git
   cd page-factory-2-example
   git remote rm origin
-  git remote add origin https://$GITHUB_AUTH_TOKEN@github.com/sbtqa/page-factory-2-example.git
+  git remote add origin https://$GITHUB_TOKEN@github.com/sbtqa/page-factory-2-example.git
   sed -i 's#<pf2.version>[0-9.]*</pf2.version>#<pf2.version>$TRAVIS_TAG</pf2.version>#' pom.xml
   git add pom.xml
   git commit -m "[skip ci] bump pf2 version"
