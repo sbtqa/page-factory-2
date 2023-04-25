a381e8c3ad23e1f16196826a274773aa2d55d1b5
Konstantin Maltsev <maltsevky@gmail.com>
2023-04-25 14:22:07 +0700

    fix travis_tag replace

--- .travis/pushback.sh
+++ .travis/pushback.sh
@@ -7,7 +7,7 @@ then
   cd page-factory-2-example
   git remote rm origin
   git remote add origin https://$GITHUB_TOKEN@github.com/sbtqa/page-factory-2-example.git
-  sed -i 's#<pf2.version>[0-9.]*</pf2.version>#<pf2.version>$TRAVIS_TAG</pf2.version>#' pom.xml
+  sed -i "s#<pf2.version>[0-9.]*</pf2.version>#<pf2.version>$TRAVIS_TAG</pf2.version>#" pom.xml
   git add pom.xml
   git commit -m "[skip ci] bump pf2 version"
   git pull origin master
