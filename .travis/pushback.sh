1712da17d672e4308a5b0486119b0e76d033c68c
Konstantin Maltsev <maltsevky@gmail.com>
2023-04-26 09:24:32 +0700

    [ci skip] revert webdriver version

--- .travis/pushback.sh
+++ .travis/pushback.sh
@@ -9,7 +9,7 @@ then
   git remote add origin https://$GITHUB_TOKEN@github.com/sbtqa/page-factory-2-example.git
   sed -i "s#<pf2.version>[0-9.]*</pf2.version>#<pf2.version>$TRAVIS_TAG</pf2.version>#" pom.xml
   git add pom.xml
-  git commit -m "[skip ci] bump pf2 version"
+  git commit -m "bump pf2 version by page-factory-2"
   git pull origin master
   git push origin master
 fi
